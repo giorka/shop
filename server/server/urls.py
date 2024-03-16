d413380086f28c67ad5b7d3439de0d97e55e23f5
giorka <mokenosov@ro.ru>
2024-03-16 21:06:12 +0300

    + Linking the application

--- server/server/urls.py
+++ server/server/urls.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
-urlpatterns = [
+django_urlpatterns = (
     path('admin/', admin.site.urls),
-]
+)
+
+urlpatterns = (
+    *django_urlpatterns,
+    path('api/v1/', include('v1.urls')),
+
+)
