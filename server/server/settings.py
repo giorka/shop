13257ba71c9212a3183772871c1be75b00eecd22
giorka <mokenosov@ro.ru>
2024-03-16 20:34:13 +0300

    + Grouping Application Names

--- server/server/settings.py
+++ server/server/settings.py
@@ -14,13 +14,18 @@ DEBUG = loads(getenv(key='DEBUG'))
 
 ALLOWED_HOSTS = []
 
-INSTALLED_APPS = [
+DJANGO_APPS = (
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+)
+
+INSTALLED_APPS = [
+    *DJANGO_APPS,
+
 ]
 
 MIDDLEWARE = [
