1249fda1dd6f4c46930a3fe2189490c73e69b71f
giorka <mokenosov@ro.ru>
2024-06-20 14:04:00 +0300

    + TokenCreateView (Login), + TokenDestroyView (Logout)

--- server/v1__auth/urls.py
+++ server/v1__auth/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
+from djoser import views as djoser
 
 from . import views
 
 urlpatterns = (
     path('register/', views.UserCreateAPIView.as_view()),
+    path('login/', djoser.TokenCreateView.as_view()),
+    path('logout/', djoser.TokenDestroyView.as_view()),
     # path('cart/', views.CartListCreateAPIView.as_view()),
 )
