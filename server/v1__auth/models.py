e23b004e3df3da608ed2a0cfbe71f7bac0cd3487
giorka <mokenosov@ro.ru>
2024-06-24 15:21:35 +0300

    / Admin Login Bug

--- server/v1__auth/models.py
+++ server/v1__auth/models.py
@@ -6,9 +6,6 @@ class User(AbstractUser):
     first_name = last_name = None  # Удаляем поля
     email = models.EmailField(unique=True)
 
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ('username',)
-
     class Meta:
         verbose_name = 'Пользователь'
         verbose_name_plural = 'Пользователи'
