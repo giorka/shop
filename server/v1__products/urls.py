7d021be348a08e5e4c5c47a9ae5c35c1309d908b
GiperBoreipy <evgenchist207@gmail.com>
2024-09-13 20:10:09 +0300

    g

--- server/v1__products/urls.py
+++ server/v1__products/urls.py
@@ -4,7 +4,7 @@ from django.views.decorators.cache import cache_page
 from . import views
 
 urlpatterns = [
-    path('', cache_page(6 * (60 * 60))(views.ProductListAPIView.as_view())),
-    path('order', views.OrderListCreateAPIView.as_view()),
-    path('<str:pk>', views.ProductRetrieveAPIView.as_view()),
+    path('/', cache_page(6 * (60 * 60))(views.ProductListAPIView.as_view())),
+    path('order/', views.OrderListCreateAPIView.as_view()),
+    path('<str:pk>/', views.ProductRetrieveAPIView.as_view()),
 ]
